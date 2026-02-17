93209edf5af082e585d3b9d3f91183b8982237ad
BozgunBer-2506 <bozgun76@gmail.com>
2026-02-17 22:08:32 +0100

    refactor: update database connection to use environment variables

--- app/database.py
+++ app/database.py
@@ -4,9 +4,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL if DATABASE_URL and "localhost" not in DATABASE_URL 
+    else "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
